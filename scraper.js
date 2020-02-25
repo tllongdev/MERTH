fd8da2344ada749078b0699ea4a9bba428234ca1
TLLong <tllongdev@gmail.com>
2020-02-25 01:32:51 -0600

    store map link clicking successful

--- scraper.js
+++ scraper.js
@@ -1,13 +1,17 @@
 const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 const iPhoneXR = devices['iPhone XR landscape'];
-const iPad = devices['iPad'];
 
 puppeteer.launch({ headless: false }).then(async browser => {
   const page = await browser.newPage();
   await page.emulate(iPhoneXR);
-  const url = 'https://www.homedepot.com/p/DAP-Alex-Plus-10-1-oz-White-Acrylic-Latex-Caulk-Plus-Silicone-18103/100097524';
+  const url =
+    'https://www.homedepot.com/p/DAP-Alex-Plus-10-1-oz-White-Acrylic-Latex-Caulk-Plus-Silicone-18103/100097524';
   await page.goto(url);
+  await Promise.all([
+    await page.click('div.store-availability__inventory > a'),
+    await page.waitForNavigation({ waitUntil: 'load' })
+  ]);
   await page.screenshot({ path: 'test.png' });
   await browser.close();
 });
